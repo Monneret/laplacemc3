ac61366d08aa2bf6e48a035dd4eaecf0da6451f4
Monneret <gilles.monneret@upmc.fr>
2017-06-19 11:08:16 +0200

    change of param: p from ss

--- R/dag_tresh.R
+++ R/dag_tresh.R
@@ -5,7 +5,7 @@
 #' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 #' @title dag_tresh
 #' @title Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...
-#' @param ss ...
+#' @param p ...
 #' @param res ...
 #' @param tresh ...
 #' @return ...
@@ -14,8 +14,8 @@
 #' #dag_tresh(ss,res,tresh)
 #'
 
-dag_tresh<-function(ss,res,tresh=0.85){
-  ep=matrix(apply(sapply(res$dag[1000:itermax],function(z) c(as.adjacency(z))),1,mean),ss$p,ss$p)
+dag_tresh<-function(p,res,tresh=0.85){
+  ep=matrix(apply(sapply(res$dag[1000:length(res$score)],function(z) c(as.adjacency(z))),1,mean),p,p)
   consensus.dag=as.bn(ep>threshold)
   return(ep=ep,consensus.dag=consensus.dag)
 }
